ee74c13a38497425df4dc429f0c9f69d168cce3d
Matt Biddulph <mb@hackdiary.com>
2017-02-17 13:30:42 -0800

    Graphql

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,35 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import withData from '../lib/withData';
 
-export default () => <div>Hello world!</div>;
+const indexQuery = gql`
+query Files {
+  files {
+    id
+    url
+    name
+  }
+}
+`;
+
+const Index = ({ data }) => {
+  if (data.loading) {
+    return <div>Hello world!</div>;
+  }
+  return (
+    <ul>
+      {data.files.map(file => <li key={file.id}>{file.name}</li>)}
+    </ul>
+  );
+};
+
+Index.propTypes = {
+  data: React.PropTypes.shape({
+    loading: React.PropTypes.bool.isRequired
+  }).isRequired
+};
+
+const IndexWithQuery = graphql(indexQuery)(Index);
+
+export default withData(() => <IndexWithQuery />);
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const next = require('next');
+const bodyParser = require('body-parser');
+const { graphqlExpress } = require('graphql-server-express');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const schema = require('./lib/schema');
+
 app.prepare().then(() => {
   const server = express();
+  server.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
   server.get('*', (req, res) => handle(req, res));
 
